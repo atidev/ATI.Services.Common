8439f7a9c663366719d6a10ab667505f87984b9d
aleksandr.p <aleksandr.p@ati.su>
2022-04-25 19:12:25 +0300

    Some upd.

--- ATI.Services.Common/Extensions/OperationResultExtensions.cs
+++ ATI.Services.Common/Extensions/OperationResultExtensions.cs
@@ -1,13 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using ATI.Services.Common.Behaviors;
 using JetBrains.Annotations;
 
 namespace ATI.Services.Common.Extensions
 {
+    /// <summary>
+    /// Расширения для OperationResult 'T
+    /// </summary>
     public static class OperationResultExtensions
     {
+        /// <summary>
+        ///  Проецирует значение исходного OperationResult в новую форму TOut с помощью функции map.
+        ///  Перемещает ошибку из исходного OperationResult в OperationResult 'TOut
+        /// </summary>
         public static OperationResult<TOut> Select<TInternal, TOut>(this OperationResult<TInternal> operationResult, [NotNull] Func<TInternal, TOut> map)
         {
             if (operationResult.Success)
@@ -15,6 +23,10 @@ namespace ATI.Services.Common.Extensions
             return new OperationResult<TOut>(operationResult);
         }
         
+        /// <summary>
+        /// Проецирует значение OperationResult в новую форму с помощью функции map.
+        /// Заменяет ошибку в исходном OperationResult на defaultValue
+        /// </summary>
         public static OperationResult<TOut> SelectOr<TInternal, TOut>(this OperationResult<TInternal> operationResult, [NotNull] Func<TInternal, TOut> map, [NotNull] TOut defaultValue)
         {
             if (operationResult.Success)
@@ -22,6 +34,10 @@ namespace ATI.Services.Common.Extensions
             return new OperationResult<TOut>(defaultValue);
         }
         
+        /// <summary>
+        /// Проецирует значение OperationResult в новую форму с помощью функции map.
+        /// Проецирует ошибку из OperationResult в новую форму с помощью функции mapError
+        /// </summary>
         public static OperationResult<TOut> SelectOrElse<TInternal, TOut>(this OperationResult<TInternal> operationResult, [NotNull] Func<TInternal, TOut> map, [NotNull] Func<ActionStatus, IList<OperationError>, TOut> mapError)
         {
             if (operationResult.Success)
@@ -29,6 +45,50 @@ namespace ATI.Services.Common.Extensions
             return new OperationResult<TOut>(mapError(operationResult.ActionStatus, operationResult.Errors));
         }
         
+        /// <summary>
+        /// Проецирует ошибку из OperationResult в новое значение с помощью функции mapErrorToFallback
+        /// </summary>
+        public static OperationResult<TInternal> Fallback<TInternal>(this OperationResult<TInternal> operationResult, [NotNull] Func<IList<OperationError>, TInternal> mapErrorToFallback)
+        {
+            if (!operationResult.Success)
+                return new OperationResult<TInternal>(mapErrorToFallback(operationResult.Errors));
+            return operationResult;
+        }
+        
+        /// <summary>
+        /// Посещает значение OperationResult c помощью inspectAction
+        /// </summary>
+        public static OperationResult<TInternal> InspectSuccess<TInternal>(this OperationResult<TInternal> operationResult, [NotNull] Action<TInternal> inspectAction)
+        {
+            if (operationResult.Success)
+                inspectAction(operationResult.Value);
+            return operationResult;
+        }
+        
+        /// <summary>
+        /// Посещает ошибку OperationResult c помощью inspectAction
+        /// </summary>
+        public static OperationResult<TInternal> InspectError<TInternal>(this OperationResult<TInternal> operationResult, [NotNull] Action<ActionStatus, IList<OperationError>> inspectAction)
+        {
+            if (!operationResult.Success)
+                inspectAction(operationResult.ActionStatus, operationResult.Errors);
+            return operationResult;
+        }
+        
+        /// <summary>
+        /// Вычисляет является операции успешной и выполняется ли для нее предикат 
+        /// </summary>
+        public static bool IsSuccessWith<TInternal>(this OperationResult<TInternal> operationResult, [NotNull] Func<TInternal, bool> predicate)
+        {
+            return operationResult.Select(predicate).Value;
+        }
+
+        #region Async
+        
+        /// <summary>
+        /// Преобразует значение OperationResult в асинхронную задачу с помощью функции map.
+        /// Перемещает ошибку из исходного OperationResult в OperationResult, получаемый в результате выполнения асинхронной операции
+        /// </summary>
         public static Task<OperationResult<TOut>> SelectAsync<TInternal, TOut>(this OperationResult<TInternal> operationResult, [NotNull] Func<TInternal, Task<OperationResult<TOut>>> map)
         {
             if (operationResult.Success)
@@ -36,6 +96,10 @@ namespace ATI.Services.Common.Extensions
             return Task.FromResult(new OperationResult<TOut>(operationResult));
         }
         
+        /// <summary>
+        /// Проецирует значение OperationResult в асинхронную задачу с помощью функции map.
+        /// Заменяет ошибку в исходном OperationResult на defaultValue
+        /// </summary>
         public static Task<OperationResult<TOut>> SelectOrAsync<TInternal, TOut>(this OperationResult<TInternal> operationResult, [NotNull] Func<TInternal, Task<OperationResult<TOut>>> map, [NotNull] TOut defaultValue)
         {
             if (operationResult.Success)
@@ -43,6 +107,10 @@ namespace ATI.Services.Common.Extensions
             return Task.FromResult(new OperationResult<TOut>(defaultValue));
         }
         
+        /// <summary>
+        /// Проецирует значение OperationResult в асинхронную задачу с помощью функции map.
+        /// Проецирует ошибку из OperationResult в асинхронную задачу с помощью функции mapError
+        /// </summary>
         public static Task<OperationResult<TOut>> SelectOrElseAsync<TInternal, TOut>(this OperationResult<TInternal> operationResult, [NotNull] Func<TInternal, Task<OperationResult<TOut>>> map, [NotNull] Func<ActionStatus, IList<OperationError>, Task<OperationResult<TOut>>> mapError)
         {
             if (operationResult.Success)
@@ -50,23 +118,16 @@ namespace ATI.Services.Common.Extensions
             return mapError(operationResult.ActionStatus, operationResult.Errors);
         }
 
-        public static OperationResult<TInternal> InspectSuccess<TInternal>(this OperationResult<TInternal> operationResult, [NotNull] Action<TInternal> action)
-        {
-            if (operationResult.Success)
-                action(operationResult.Value);
-            return operationResult;
-        }
-        
-        public static OperationResult<TInternal> InspectError<TInternal>(this OperationResult<TInternal> operationResult, [NotNull] Action<ActionStatus, IList<OperationError>> action)
+        /// <summary>
+        /// Проецирует ошибку из OperationResult в асинхронную операцию с помощью функции mapErrorToFallback
+        /// </summary>
+        public static Task<OperationResult<TInternal>> FallbackAsync<TInternal>(this OperationResult<TInternal> operationResult, [NotNull] Func<IList<OperationError>, Task<OperationResult<TInternal>>> mapErrorToFallback)
         {
             if (!operationResult.Success)
-                action(operationResult.ActionStatus, operationResult.Errors);
-            return operationResult;
+                return mapErrorToFallback(operationResult.Errors);
+            return Task.FromResult(operationResult);
         }
-        
-        public static bool IsSuccessWith<TInternal>(this OperationResult<TInternal> operationResult, [NotNull] Func<TInternal, bool> predicate)
-        {
-            return operationResult.Select(predicate).Value;
-        }
+
+        #endregion
     }
 }
