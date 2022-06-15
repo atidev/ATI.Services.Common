1b28c2d87e0a204d4f1e08fe62501b9b0639b866
Artem Kudriashov <artem@MacBook-Pro-Artem.local>
2022-06-15 12:25:01 +0400

    default flow context current value

--- ATI.Services.Common/Context/FlowContext.cs
+++ ATI.Services.Common/Context/FlowContext.cs
@@ -4,13 +4,18 @@ using JetBrains.Annotations;
 namespace ATI.Services.Common.Context
 {
     [PublicAPI]
-    public static class FlowContext<T>
+    public static class FlowContext<T> where T : new()
     {
         private static readonly AsyncLocal<T> AsyncLocal = new();
 
         public static T Current
         {
-            get => AsyncLocal.Value;
+            get
+            {
+                if (AsyncLocal.Value != null)
+                    return AsyncLocal.Value;
+                return AsyncLocal.Value = new ();
+            }
             set => AsyncLocal.Value = value;
         }
     }
