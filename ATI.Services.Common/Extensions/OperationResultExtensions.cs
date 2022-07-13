65198bb3e6dfc1ed5bea1cb4698eaa5e897e551f
aleksandr.p <aleksandr.p@ati.su>
2022-07-13 22:13:45 +0300

    Ref.

--- ATI.Services.Common/Extensions/OperationResultExtensions.cs
+++ ATI.Services.Common/Extensions/OperationResultExtensions.cs
@@ -6,38 +6,38 @@ using JetBrains.Annotations;
 
 namespace ATI.Services.Common.Extensions
 {
-    public static class OperationResultExtensions 
+    public static class OperationResultExtensions
     {
         public static ILazyEvaluate<TResult> Map<TSource, TResult>(this OperationResult<TSource> source, Func<TSource, TResult> map)
         {
             return new OperationResultSelector<TSource, TResult>(source, map);
         }
-        
+
         public static ILazyEvaluate<TResult> Map2<TFirst, TSecond, TResult>(this OperationResult<TFirst> first, OperationResult<TSecond> second, Func<TFirst, TSecond, TResult> map2)
         {
             return first.Map(f => second.Map(s => map2(f, s))).UnwrapLazy(second);
         }
-        
+
         public static ILazyEvaluate<TResult> Map2<TFirst, TSecond, TResult>(this OperationResult<TFirst> first, ILazyEvaluate<TSecond> second, Func<TFirst, TSecond, TResult> map2)
         {
             return first.Map(f => second.Map(s => map2(f, s))).UnwrapLazy(second.GetInitialOperationResult());
         }
-        
+
         public static ILazyEvaluateAsync<TResult> MapAsync<TSource, TResult>(this OperationResult<TSource> source, Func<TSource, Task<TResult>> map)
         {
             return new OperationResultAsyncSelector<TSource, TResult>(source, map);
         }
-        
+
         public static ILazyEvaluateAsync<TResult> Map2Async<TFirst, TSecond, TResult>(this OperationResult<TFirst> first, OperationResult<TSecond> second, Func<TFirst, TSecond, Task<TResult>> map2)
         {
             return first.Map2(second, map2).AsAsync();
         }
-        
+
         public static ILazyEvaluateAsync<TResult> Map2Async<TFirst, TSecond, TResult>(this OperationResult<TFirst> first, ILazyEvaluate<TSecond> second, Func<TFirst, TSecond, Task<TResult>> map2)
         {
             return first.Map2(second, map2).AsAsync();
         }
-        
+
         public static OperationResult<TSource> Fallback<TSource>(this OperationResult<TSource> source, Func<ActionStatus, IList<OperationError>, OperationResult<TSource>> fallback)
         {
             return !source.Success ? fallback(source.ActionStatus, source.Errors) : source;
@@ -49,7 +49,7 @@ namespace ATI.Services.Common.Extensions
         }
 
         /// <summary>
-        /// Посещает значение OperationResult c помощью inspectAction
+        ///     Посещает значение OperationResult c помощью inspectAction
         /// </summary>
         public static OperationResult<TSource> InspectSuccess<TSource>(this OperationResult<TSource> source, [NotNull] Action<TSource> inspectAction)
         {
@@ -59,9 +59,9 @@ namespace ATI.Services.Common.Extensions
             inspectAction(source.Value);
             return source;
         }
-        
+
         /// <summary>
-        /// Посещает значение OperationResult c помощью inspectAction
+        ///     Посещает значение OperationResult c помощью inspectAction
         /// </summary>
         public static OperationResult<TSource> InspectError<TSource>(this OperationResult<TSource> source, [NotNull] Action<ActionStatus, IList<OperationError>> inspectAction)
         {
@@ -73,13 +73,11 @@ namespace ATI.Services.Common.Extensions
         }
 
         /// <summary>
-        /// Вычисляет является операции успешной и выполняется ли для нее предикат 
+        ///     Вычисляет является операции успешной и выполняется ли для нее предикат
         /// </summary>
         public static bool IsSuccessWith<TSource>(this OperationResult<TSource> source, [NotNull] Func<TSource, bool> predicate)
         {
-            if (source.Success)
-                return predicate(source.Value);
-            return false;
+            return source.Success && predicate(source.Value);
         }
 
         public static OperationResult<TValue> Unwrap<TValue>(this OperationResult<OperationResult<TValue>> source)
