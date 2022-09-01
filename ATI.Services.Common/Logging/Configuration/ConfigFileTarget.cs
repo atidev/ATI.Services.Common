3795acb9c01a4c03cbfd4125e820e8dc9a381c7f
Aidar.Shaikhiev <aidar.s@ati.su>
2022-09-01 19:04:47 +0300

    Save logs to project folder, instead of bin folder

--- ATI.Services.Common/Logging/Configuration/ConfigFileTarget.cs
+++ ATI.Services.Common/Logging/Configuration/ConfigFileTarget.cs
@@ -6,11 +6,11 @@ namespace ATI.Services.Common.Logging.Configuration
     {
         public string Name { get; set; }
         public int MaxArchiveDays { get; set; } = 7;
-        public string FileName { get; set; } = "${basedir}/Log/NLog.Errors.json";
+        public string FileName { get; set; } = "${currentdir}/Log/NLog.Errors.json";
         public ArchiveNumberingMode ArchiveNumbering { get; set; } = ArchiveNumberingMode.Date;
         public FileArchivePeriod ArchiveEvery { get; set; } = FileArchivePeriod.Day;
         public string ArchiveDateFormat { get; set; } = "yyyyMMdd";
-        public string ArchiveFileName { get; set; } = "${basedir}/Log/NLog.Error.{##}.json";
+        public string ArchiveFileName { get; set; } = "${currentdir}/Log/NLog.Error.{##}.json";
         public bool AddGeneralAttributes { get; set; } = true;
         public ConfigJsonAttribute[] Attributes { get; set; } = { };
     }
