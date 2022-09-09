a2c313bfd1a9704e7426be36f8866916e5ec87c7
aleksandr.p <aleksandr.p@ati.su>
2022-09-09 15:07:26 +0300

    Task<T> to OperationExecutor extension

--- ATI.Services.Common/Extensions/TaskExtensions.cs
+++ ATI.Services.Common/Extensions/TaskExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using ATI.Services.Common.Behaviors;
 
 namespace ATI.Services.Common.Extensions
 {
@@ -6,6 +7,11 @@ namespace ATI.Services.Common.Extensions
     {
         public static void Forget(this Task task)
         {
+        }
+
+        public static IOperationExecutorAsync<OperationResult<TResult>> ToOperationExecutor<TResult>(this Task<OperationResult<TResult>> task)
+        {
+            return new OperationResultAsyncExecutor<int, OperationResult<TResult>>(new OperationResult<int>(OperationResult.Ok), _ => task);
         }
     }
 }
