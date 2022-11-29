91c07fc13ba51be076cfba2838be5ea0f6a51b6f
Aidar.Shaikhiev <aidar.s@ati.su>
2022-11-29 16:24:03 +0300

    LogLevelOverride in BaseServiceOptions.cs

--- ATI.Services.Common/Options/BaseServiceOptions.cs
+++ ATI.Services.Common/Options/BaseServiceOptions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using ATI.Services.Common.Serializers;
 using JetBrains.Annotations;
+using NLog;
 
 namespace ATI.Services.Common.Options;
 
@@ -10,7 +11,6 @@ public class BaseServiceOptions
 {
     public string ConsulName { get; set; }
     public TimeSpan TimeOut { get; set; }
-    public bool LogTimeoutsAsWarn { get; set; }
     public string Environment { get; set; }
     public TimeSpan? LongRequestTime { get; set; }
 
@@ -19,4 +19,5 @@ public class BaseServiceOptions
     public bool AddCultureToRequest { get; set; } = true;
     public List<string> HeadersToProxy { get; set; } = new();
     public SerializerType SerializerType { get; set; } = SerializerType.Newtonsoft;
+    public virtual Func<LogLevel, LogLevel> LogLevelOverride => level => level;
 }
