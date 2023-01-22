5a27bc5f79e9dc0cab77c144d1471b7bc00c0aba
artem.s <artem.s@ati.su>
2023-01-22 18:32:39 +0100

    add OperationResultCollectionExtensions

---
+++ ATI.Services.Common/Extensions/CollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using ATI.Services.Common.Behaviors;
+using JetBrains.Annotations;
+using Polly.Caching;
+
+namespace ATI.Services.Common.Extensions;
+
+[PublicAPI]
+public static class OperationResultCollectionExtensions
+{
+    public static async void Add<T>(this ICollection<OperationResult> collection, Task<OperationResult<T>> task) 
+        => collection.Add(await task);
+
+    public static bool Success(this ICollection<OperationResult> collection) 
+        => collection.All(i => i.Success);
+
+    public static OperationResult GetNotSuccessOperation(this ICollection<OperationResult> collection) 
+        => collection.FirstOrDefault(i => !i.Success);
+}
