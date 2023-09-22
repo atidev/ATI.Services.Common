044738bacd0b05d38893524bd487899f45b799e8
Станислав Терещенков <tereschenkov.s@ati.su>
2023-09-22 17:52:25 +0300

    update readme

--- ATI.Services.Common/Caching/Redis/RedisOptions.cs
+++ ATI.Services.Common/Caching/Redis/RedisOptions.cs
@@ -17,8 +17,20 @@ namespace ATI.Services.Common.Caching.Redis
         public TimeSpan? LongRequestTime { get; set; }
         public bool MustConnectOnInit { get; set; }
         
-        public string Server { get; set; }
+        /// <summary>
+        /// Хосты. Если их несколько, перечислять через запятую
+        /// </summary>
+        public string Servers { get; set; }
+        
+        /// <summary>
+        /// Имя redis в Sentinel
+        /// </summary>
         public string ServiceName { get; set; }
+        
+        /// <summary>
+        /// Таймаут к операциям редиса.
+        /// Лучше указывать его меньше, чем RedisTimeout - в этом случае в логи падают развернутые ошибки от Redis, что пошло не так
+        /// </summary>
         public int? SyncTimeoutMs { get; set; }
 
         public SerializerType Serializer { get; set; } = SerializerType.SystemTextJson;
@@ -29,10 +41,10 @@ namespace ATI.Services.Common.Caching.Redis
                 ? ConfigurationOptions.Parse(ConnectionString)
                 : new ConfigurationOptions();
             
-            if (Server != null)
+            if (Servers != null)
             {
                 options.EndPoints.Clear();
-                options.EndPoints.Add(Server);
+                options.EndPoints.Add(Servers);
             }
 
             if (ServiceName != null)
