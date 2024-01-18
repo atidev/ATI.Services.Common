770d7cc638defa1fbd61f85da09f9e842a6c42cf
Stanislav Tereschenkov <Shel_Stas@mail.ru>
2024-01-18 12:26:43 +0300

    Add trace fields to logs (#70)
    * add trace fields to log
    
    * add traceId
    
    * remove traceId because its useless
    
    ---------
    
    Co-authored-by: Станислав Терещенков <tereschenkov.s@ati.su>

--- ATI.Services.Common/Logging/NLogConfigurator.cs
+++ ATI.Services.Common/Logging/NLogConfigurator.cs
@@ -32,7 +32,11 @@ namespace ATI.Services.Common.Logging
             JsonAttributeHelper.CreateWithoutUnicodeEscaping("exceptionPretty", "${onexception:${exception:format=ToString,Data:exceptionDataSeparator=\\r\\n}}"),
             JsonAttributeHelper.CreateWithoutUnicodeEscaping("logContext", "${event-properties:logContext}"),
             JsonAttributeHelper.CreateWithoutUnicodeEscaping("metricString", "${event-properties:metricString}"),
-            JsonAttributeHelper.CreateWithoutUnicodeEscaping("metricSource", "${event-properties:metricSource}")
+            JsonAttributeHelper.CreateWithoutUnicodeEscaping("metricSource", "${event-properties:metricSource}"),
+            JsonAttributeHelper.CreateWithoutUnicodeEscaping("traceId", "${activity:property=TraceId}"),
+            JsonAttributeHelper.CreateWithoutUnicodeEscaping("traceParentId", "${activity:property=ParentId}"),
+            JsonAttributeHelper.CreateWithoutUnicodeEscaping("traceSpanId", "${activity:property=SpanId}"),
+            JsonAttributeHelper.CreateWithoutUnicodeEscaping("traceState", "${activity:property=TraceState}")
         };
 
         public NLogConfigurator(NLogOptions options)
