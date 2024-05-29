5afdbab899c054e5e4da04e6003e582a7102ce86
Батура Иван <batura.i@MacBook-Pro-Komov.local>
2024-05-29 15:39:02 +0300

    Extend loggin for rotation

--- ATI.Services.Common/Sql/PostgresDapperProvider.cs
+++ ATI.Services.Common/Sql/PostgresDapperProvider.cs
@@ -1,5 +1,6 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using ATI.Services.Common.Logging;
 using ATI.Services.Common.Metrics;
 using JetBrains.Annotations;
 using Microsoft.Extensions.Options;
@@ -24,23 +25,34 @@ public class PostgresDapperProvider
         dbManagerOptions.OnChange(o => GetConfiguredDataBases(o.DataBaseOptions, metricsFactory));
     }
 
-    private void GetConfiguredDataBases(Dictionary<string, DataBaseOptions> dataBaseOptions, MetricsFactory metricsFactory)
+    private void GetConfiguredDataBases(Dictionary<string, DataBaseOptions> newDataBaseOptions, MetricsFactory metricsFactory)
     {
-        foreach (var kvDataBaseOptions in dataBaseOptions)
+        _logger.WarnWithObject("New database options", new { newDataBaseOptions });
+        
+        foreach (var (newPostgresName, newDataBaseOption) in newDataBaseOptions)
         {
-            if (_configuredDataBases.TryGetValue(kvDataBaseOptions.Key, out var config))
+            if (_configuredDataBases.TryGetValue(newPostgresName, out var oldDatabase))
             {
                 //Если кто-то сменил change token IOptionsMonitor, и не сменил креды от бд, не нужно сбрасывать коннекшен пул
-                if (config.Options.UserName == kvDataBaseOptions.Value.UserName 
-                    && config.Options.Password == kvDataBaseOptions.Value.Password)
+                if (oldDatabase.Options.UserName == newDataBaseOption.UserName 
+                    && oldDatabase.Options.Password == newDataBaseOption.Password)
                     continue;
                 
-                NpgsqlConnection.ClearPool(new NpgsqlConnection(ConnectionStringBuilder.BuildPostgresConnectionString(_configuredDataBases[kvDataBaseOptions.Key].Options)));
-                config.Options = kvDataBaseOptions.Value;
-                
+                _logger.WarnWithObject("Rotate postgres credentials on common", new
+                {
+                    RotateOptions = newDataBaseOption, 
+                    oldDatabase.Options,
+                });
+                NpgsqlConnection.ClearPool(new NpgsqlConnection(ConnectionStringBuilder.BuildPostgresConnectionString(oldDatabase.Options)));
+                oldDatabase.Options = newDataBaseOption;
+                _logger.WarnWithObject("Postgres credentials after rotate", new
+                {
+                    RotateOptions = newDataBaseOption, 
+                    oldDatabase.Options,
+                });
             }
             else
-                _configuredDataBases.TryAdd(kvDataBaseOptions.Key, new PostgresDapper(kvDataBaseOptions.Value, metricsFactory));
+                _configuredDataBases.TryAdd(newPostgresName, new PostgresDapper(newDataBaseOption, metricsFactory));
         }
     }
 
