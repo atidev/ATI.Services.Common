ef25a0d3b273eea1694b704c7be431979438cdd2
Швед Дмитрий <shved.d@ati.su>
2024-07-17 18:28:50 +0300

    Reapply "Add mattermost bot message"
    This reverts commit 2379fc007093ed6b93a044f6ad17d75fcc29acc9.

--- ATI.Services.Common/Mattermost/MattermostExtensions.cs
+++ ATI.Services.Common/Mattermost/MattermostExtensions.cs
@@ -8,6 +8,9 @@ public static class MattermostExtensions
     public static IServiceCollection AddMattermost(this IServiceCollection services)
     {
         services.ConfigureByName<MattermostProviderOptions>();
+
+        services.AddHttpClient(MattermostAdapter.HttpClientName);
+        
         return services
             .AddSingleton<MattermostProvider>();
     }
--- ATI.Services.Common/Mattermost/MattermostOptions.cs
+++ ATI.Services.Common/Mattermost/MattermostOptions.cs
@@ -6,4 +6,5 @@ public class MattermostOptions
     public string IconEmoji { get; set; }
     public string MattermostAddress { get; set; }
     public string WebHook { get; set; }
+    public string BotAccessToken { get; set; }
 }
